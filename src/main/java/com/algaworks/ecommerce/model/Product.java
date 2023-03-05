5fd833ab9d47ec87006d4b5ff83d36db2d5fa2fc
Danilo N. B. <danilobenatti@gmail.com>
2023-03-05 00:03:51 -0300

    Starting whit Jakarta Persistence

---
+++ src/main/java/com/algaworks/ecommerce/model/Product.java
@@ -0,0 +1,81 @@
+package com.algaworks.ecommerce.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	private Integer id;
+	
+	private String name;
+	
+	private String description;
+	
+	private BigDecimal price;
+	
+	public Product() {
+	}
+	
+	public Product(Integer id, String name, String description,
+			BigDecimal price) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Product)) {
+			return false;
+		}
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
