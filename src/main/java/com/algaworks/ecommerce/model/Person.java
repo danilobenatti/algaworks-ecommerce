87b1bd68119181a69376a7fa651ce7c26dd73faa
Danilo N. B. <danilobenatti@gmail.com>
2023-03-06 14:05:35 -0300

    Knowing and using Lombok.

--- src/main/java/com/algaworks/ecommerce/model/Person.java
+++ src/main/java/com/algaworks/ecommerce/model/Person.java
@@ -1,83 +1,34 @@
 package com.algaworks.ecommerce.model;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 @Entity
 public class Person {
 	
 	@Id
+	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String name;
 	
 	private LocalDate birthday;
-	
-	public Person() {
-	}
-	
-	public Person(Integer id, String name, LocalDate birthday) {
-		this.id = id;
-		this.name = name;
-		this.birthday = birthday;
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public LocalDate getBirthday() {
-		return birthday;
-	}
-	
-	public void setBirthday(LocalDate birthday) {
-		this.birthday = birthday;
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!(obj instanceof Person))
-			return false;
-		Person other = (Person) obj;
-		return Objects.equals(id, other.id);
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Person [id=");
-		builder.append(id);
-		builder.append(", name=");
-		builder.append(name);
-		builder.append(", birthday=");
-		builder.append(birthday);
-		builder.append("]");
-		return builder.toString();
-	}
 	
 }
--- src/main/java/com/algaworks/ecommerce/model/Product.java
+++ src/main/java/com/algaworks/ecommerce/model/Product.java
@@ -1,17 +1,29 @@
 package com.algaworks.ecommerce.model;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 @Entity
 public class Product {
 	
 	@Id
+	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
@@ -20,80 +32,5 @@ public class Product {
 	private String description;
 	
 	private BigDecimal price;
-	
-	public Product() {
-	}
-	
-	public Product(Integer id, String name, String description,
-			BigDecimal price) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.price = price;
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	public BigDecimal getPrice() {
-		return price;
-	}
-	
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Product)) {
-			return false;
-		}
-		Product other = (Product) obj;
-		return Objects.equals(id, other.id);
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Product [id=");
-		builder.append(id);
-		builder.append(", name=");
-		builder.append(name);
-		builder.append(", description=");
-		builder.append(description);
-		builder.append(", price=");
-		builder.append(price);
-		builder.append("]");
-		return builder.toString();
-	}
 	
 }
