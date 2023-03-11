ee8e3c271d9bf55329cb5d0a8da41ebf47a81933
Danilo N. B. <danilobenatti@gmail.com>
2023-03-11 16:07:36 -0300

    Mapping one-to-one relationships with @JoinTable

--- src/main/java/com/algaworks/ecommerce/model/Invoice.java
+++ src/main/java/com/algaworks/ecommerce/model/Invoice.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.model;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
@@ -10,9 +11,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -25,9 +26,9 @@ import lombok.Setter;
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table(name = "tbl_invoices",
-	uniqueConstraints = @UniqueConstraint(name = "uk_invoice_order_id",
-		columnNames = "oreder_id"))
+@Table(name = "tbl_invoices")
+//	uniqueConstraints = @UniqueConstraint(name = "uk_invoice_order_id",
+//		columnNames = "oreder_id"))
 public class Invoice implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -36,9 +37,14 @@ public class Invoice implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@OneToOne
-	@JoinColumn(name = "oreder_id",
-		foreignKey = @ForeignKey(name = "fk_invoice_order_id"))
+	@OneToOne(cascade = CascadeType.REMOVE)
+//	@JoinColumn(name = "oreder_id",
+//		foreignKey = @ForeignKey(name = "fk_invoice_order_id"))
+	@JoinTable(name = "tbl_order_invoice",
+		joinColumns = @JoinColumn(name = "invoice_id", unique = true,
+			foreignKey = @ForeignKey(name = "fk_orderinvoice__invoice_id")),
+		inverseJoinColumns = @JoinColumn(name = "order_id", unique = true,
+			foreignKey = @ForeignKey(name = "fk_orderinvoice__order_id")))
 	private Order order;
 	
 	@Column(name = "col_xml")
