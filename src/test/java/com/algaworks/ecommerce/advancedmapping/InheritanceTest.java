4c9e9b3e4c64f240f6de8bec66ca9dbaeebc7598
Danilo N. B <danilobenatti@gmail.com>
2023-04-02 12:04:31 -0300

    Adjusting code for payment class

--- src/test/java/com/algaworks/ecommerce/advancedmapping/InheritanceTest.java
+++ src/test/java/com/algaworks/ecommerce/advancedmapping/InheritanceTest.java
@@ -1,32 +1,38 @@
 package com.algaworks.ecommerce.advancedmapping;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Order;
-import com.algaworks.ecommerce.model.PaymentCreditCard;
+import com.algaworks.ecommerce.model.PaymentBankSlip;
 import com.algaworks.ecommerce.model.enums.PaymentStatus;
 
 class InheritanceTest extends EntityManagerTest {
 	
 	@Test
 	void IncludePaymentOfOrder() {
-		Order order = entityManager.find(Order.class, 1L);
+		Order order = entityManager.find(Order.class, 2L);
 		
-		PaymentCreditCard creditCard = new PaymentCreditCard();
-		creditCard.setOrder(order);
-		creditCard.setNumberOfInstallments(12);
-		creditCard.setStatus(PaymentStatus.PROCESSING);
+		PaymentBankSlip bankSlip = new PaymentBankSlip();
+		bankSlip.setOrder(order);
+		bankSlip.setExpirationDate(LocalDate.now());
+		bankSlip.setPayDay(bankSlip.getExpirationDate().minusDays(3));
+		bankSlip.setStatus(PaymentStatus.PROCESSING);
+		
+		order.setPayment(bankSlip);
 		
 		entityManager.getTransaction().begin();
-		entityManager.persist(creditCard);
+		entityManager.persist(bankSlip);
 		entityManager.getTransaction().commit();
 		
 		entityManager.clear();
 		
 		Order findOrder = entityManager.find(Order.class, order.getId());
-		Assertions.assertNotNull(findOrder.getPayment());
+		Assertions.assertEquals(Byte.valueOf("1"),
+			findOrder.getPayment().getStatus().getCode());
 		
 	}
 	
