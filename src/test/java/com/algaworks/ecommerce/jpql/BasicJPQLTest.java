e990c06338c3e8333f8f2e4d4d20fb014204f05b
Danilo N. B. <danilobenatti@gmail.com>
2023-04-23 20:30:59 -0300

    Selecting an entity attribute as query return

--- src/test/java/com/algaworks/ecommerce/jpql/BasicJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicJPQLTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Order;
+import com.algaworks.ecommerce.model.Person;
 
 import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
@@ -18,7 +20,7 @@ class BasicJPQLTest extends EntityManagerTest {
 	void searchById() {
 		/*
 		 * JPQL - select o from Order o join o.orderitems i where i.product.unitPrice > 10 
-		 * SQL - select o.* from tbl_orders o where o.id = 1
+		 * SQL - select o.* from tbl_orders o where  o.id = 1
 		 */
 		String qlString = "select o from Order o where o.id = 1";
 		TypedQuery<Order> typedQuery = entityManager.createQuery(qlString,
@@ -48,4 +50,19 @@ class BasicJPQLTest extends EntityManagerTest {
 		
 	}
 	
+	@Test
+	void selectAttributeToReturn() {
+		String jpql = "select p.name from Product p";
+		TypedQuery<String> typedQuery = entityManager.createQuery(jpql,
+			String.class);
+		List<String> list = typedQuery.getResultList();
+		assertEquals(String.class, list.get(0).getClass());
+		
+		String jpql2 = "select o.person from Order o";
+		TypedQuery<Person> typedQuery2 = entityManager.createQuery(jpql2,
+			Person.class);
+		List<Person> list2 = typedQuery2.getResultList();
+		assertEquals(Person.class, list2.get(0).getClass());
+		
+	}
 }
