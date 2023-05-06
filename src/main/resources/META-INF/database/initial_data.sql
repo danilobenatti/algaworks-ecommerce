5483374675a9e1018cbaf5b6c08bf2bf6e572b7b
Danilo N. B. <danilobenatti@gmail.com>
2023-05-06 14:15:44 -0300

    Using the "where" clause with "group by"

--- src/main/resources/META-INF/database/initial_data.sql
+++ src/main/resources/META-INF/database/initial_data.sql
@@ -7,10 +7,10 @@ INSERT INTO tbl_categories (id, col_name, parent_category_id) VALUES (6, 'Notebo
 INSERT INTO tbl_categories (id, col_name, parent_category_id) VALUES (7, 'Smartphones', 1);
 INSERT INTO tbl_categories (id, col_name, parent_category_id) VALUES (8, 'Câmeras', 1);
 --
-INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (1, 'Conheça o novo Kindle, agora com mais memória', 'Kindle', 1, 499.5, date_sub(now(), interval 1 day));
-INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (3, 'Câmera de ação e alto desempenho', 'Câmera GoPro Hero', 1, 1506.72, date_sub(now(), interval 2 day));
-INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (4, 'Fita Adesiva Alta Aderência','Fital ColaTudo', 1, 5.5, date_sub(now(), interval 3 day));
-INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (5, 'Corda de Tecido Poliester Trançada Reforçada','Corda de Nylon para Varal', 3, 2.32, date_sub(now(), interval 4 day));
+INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (1, 'Conheça o novo Kindle, agora com mais memória', 'Kindle', 1, 499.5, date_sub(now(), interval(1) year));
+INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (3, 'Câmera de ação e alto desempenho', 'Câmera GoPro Hero', 1, 1506.72, date_sub(now(), interval(2) month));
+INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (4, 'Fita Adesiva Alta Aderência','Fital ColaTudo', 1, 5.5, date_sub(now(), interval(3) day));
+INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (5, 'Corda de Tecido Poliester Trançada Reforçada','Corda de Nylon para Varal', 3, 2.32, date_sub(now(), interval(4) week));
 INSERT INTO tbl_products (id, col_description, col_name, col_unit, col_unitprice, col_date_create) VALUES (6, 'O melhor ajuste de foco','Câmera Canon 80D', 1, 3500.0, sysdate());
 INSERT INTO tbl_product_attribute (product_id, col_description, col_value) VALUES (3, 'First attribute', 'Powerfull');
 INSERT INTO tbl_product_attribute (product_id, col_description, col_value) VALUES (4, 'Manufacturer', '6M');
@@ -25,25 +25,25 @@ INSERT INTO tbl_product_category (product_id, category_id) VALUES (3, 8);
 INSERT INTO tbl_product_category (product_id, category_id) VALUES (4, 3);
 INSERT INTO tbl_product_category (product_id, category_id) VALUES (6, 8);
 --
-INSERT INTO tbl_persons (id, col_firstname, col_taxidnumber, col_date_create) VALUES (1, 'Luiz Fernando', '21470959828', date_sub(now(), interval 3 day));
+INSERT INTO tbl_persons (id, col_firstname, col_taxidnumber, col_date_create) VALUES (1, 'Luiz Fernando', '21470959828', date_sub(now(), interval(3) day));
 INSERT INTO tbl_person_detail (person_id, col_birthday, col_gender) VALUES (1, '1958-10-05', 'MALE');
 INSERT INTO tbl_person_phones (person_id, col_number, col_type) VALUES (1, '+55(11)97777-6666', 'M');
 --
-INSERT INTO tbl_persons (id, col_firstname, col_taxidnumber, col_date_create) VALUES(2, 'João Marcos', '54254667817', date_sub(now(), interval 4 day));
+INSERT INTO tbl_persons (id, col_firstname, col_taxidnumber, col_date_create) VALUES(2, 'João Marcos', '54254667817', date_sub(now(), interval(4) week));
 INSERT INTO tbl_person_detail (person_id, col_birthday, col_gender) VALUES (2, '1974-05-17', 'MALE');
 INSERT INTO tbl_person_phones (person_id, col_number, col_type) VALUES (2, '(049)9 4444-3333', 'H');
 --
-INSERT INTO tbl_persons (id, col_firstname, col_taxidnumber, col_date_create) VALUES(3, 'Maria Paula', '53558795008', date_sub(now(), interval 8 day));
+INSERT INTO tbl_persons (id, col_firstname, col_taxidnumber, col_date_create) VALUES(3, 'Maria Paula', '53558795008', date_sub(now(), interval(8) month));
 INSERT INTO tbl_person_detail (person_id, col_birthday, col_gender) VALUES (3, '1963-08-21', 'FEMALE');
 INSERT INTO tbl_person_phones (person_id, col_number, col_type) VALUES (3, '+55(011)3232-4545', 'W');
 --
-INSERT INTO tbl_orders (id, col_date_create, col_date_update, col_execution_date, col_status, col_total, person_id) VALUES(1, date_sub(now(), interval 2 month), null, null, 1, 2006.22, 1);
+INSERT INTO tbl_orders (id, col_date_create, col_date_update, col_execution_date, col_status, col_total, person_id) VALUES(1, date_sub(now(), interval 2 day), null, null, 1, 2006.22, 1);
 INSERT INTO tbl_order_items (col_quantity, col_subtotal, order_id, product_id) VALUES (1, 499.5, 1, 1);
 INSERT INTO tbl_order_items (col_quantity, col_subtotal, order_id, product_id) VALUES (1, 5.5, 1, 4);
 INSERT INTO tbl_payments (order_id, col_status) VALUES (1, 1);
 INSERT INTO tbl_payments_creditcard (col_number_installments, order_id) VALUES (6, 1);
 --
-INSERT INTO tbl_orders (id, col_date_create, col_date_update, col_execution_date, col_status, col_total, person_id) VALUES (2, date_sub(now(), interval 2 month), null, null, 1, 1512.22, 2);
+INSERT INTO tbl_orders (id, col_date_create, col_date_update, col_execution_date, col_status, col_total, person_id) VALUES (2, date_sub(now(), interval 2 week), null, null, 2, 1512.22, 2);
 INSERT INTO tbl_order_items (col_quantity, col_subtotal, order_id, product_id) VALUES (1, 1506.72, 2, 3);
 INSERT INTO tbl_order_items (col_quantity, col_subtotal, order_id, product_id) VALUES (1, 5.5, 2, 4);
 --
@@ -53,8 +53,9 @@ INSERT INTO tbl_payments (order_id, col_status) VALUES (3, 1);
 INSERT INTO tbl_payments_creditcard (col_number_installments, order_id) VALUES (2, 3);
 INSERT INTO tbl_invoices (order_id, col_issuedatetime, col_xml) VALUES (3, sysdate(), '<xml />');
 --
-INSERT INTO tbl_orders (id, col_date_create, col_date_update, col_execution_date, col_status, col_total, person_id) VALUES(4, date_sub(now(), interval 8 month), null, null, 2, 3500.0, 3);
+INSERT INTO tbl_orders (id, col_date_create, col_date_update, col_execution_date, col_status, col_total, person_id) VALUES(4, date_sub(now(), interval 1 year), null, null, 3, 3500.0, 3);
 INSERT INTO tbl_order_items (col_quantity, col_subtotal, order_id, product_id) VALUES (1, 3500.0, 4, 6);
-INSERT INTO tbl_payments (order_id, col_date_create, col_status) VALUES (4, sysdate(), 1);
+INSERT INTO tbl_payments (order_id, col_date_create, col_status) VALUES (4, sysdate(), 3);
 INSERT INTO tbl_payments_bankslip (col_expirationdate, col_payday, order_id) VALUES (date_add(now(), interval 5 day), null, 4);
 INSERT INTO tbl_invoices (order_id, col_issuedatetime, col_xml) VALUES (4, sysdate(), '<xml />');
+--
--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -26,7 +26,7 @@ class GroupByTest extends EntityManagerTest {
 			+ "group by year(o.dateCreate), month(o.dateCreate)",
 		"select c.name, sum(i.subtotal) from OrderItem i join i.product p join p.categories c "
 			+ "group by c.id",
-		"select p.firstname, sum(o.total) from Order o join o.person p group by p.id ",
+		"select p.firstname, sum(o.total) from Order o join o.person p group by p.id",
 		"select date_format(o.dateCreate, '%d/%m/%Y'), concat(c.name, ': ', sum(i.subtotal)) from OrderItem i "
 			+ "join i.order o join i.product p join p.categories c "
 			+ "group by year(o.dateCreate), month(o.dateCreate), day(o.dateCreate), c.id "
@@ -47,4 +47,32 @@ class GroupByTest extends EntityManagerTest {
 			String.format("%s, %s", i[0], i[1].toString())));
 		
 	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {
+		"select date_format(o.dateCreate, '%Y/%M'), sum(o.total) from Order o "
+			+ "where year(o.dateCreate) = year(current_date) and o.status = 1 "
+			+ "group by year(o.dateCreate), month(o.dateCreate)",
+		"select c.name, sum(i.subtotal) from OrderItem i join i.product p join p.categories c join i.order o "
+			+ "where year(o.dateCreate) = year(current_date) and month(o.dateCreate) = month(current_date) "
+			+ "group by c.id",
+		"select p.firstname, sum(o.total) from Order o join o.person p "
+			+ "where year(o.dateCreate) = year(current_date) and month(o.dateCreate) >= (month(current_date) - 3)"
+			+ "group by p.id " })
+	void groupAndFilterResult(String jpql) {
+		// total of sales by month into last year and status 1 (Waiting)
+		// total of sales by category into last year and last month
+		// total of sales by person in the last 3 months
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql,
+			Object[].class);
+		
+		List<Object[]> resultList = typedQuery.getResultList();
+		
+		assertFalse(resultList.isEmpty());
+		
+		resultList.forEach(i -> logger.log(Level.INFO, "{0}",
+			String.format("%s, %s", i[0], i[1].toString())));
+		
+	}
+	
 }
