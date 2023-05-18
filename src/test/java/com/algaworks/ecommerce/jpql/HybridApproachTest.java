f2d02e1f6a9bf565a455ccf89e4c27e365fdbc7a
Danilo N. B. <danilobenatti@gmail.com>
2023-05-18 10:08:13 -0300

    Hybrid approach to dynamic and named queries

---
+++ src/test/java/com/algaworks/ecommerce/jpql/HybridApproachTest.java
@@ -0,0 +1,45 @@
+package com.algaworks.ecommerce.jpql;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Category;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+class HybridApproachTest extends EntityManagerTest {
+	
+	@BeforeAll
+	public static void setUpBeforeClass() {
+		entityManagerFactory = Persistence
+			.createEntityManagerFactory("algaworks-ecommerce");
+		EntityManager em = entityManagerFactory.createEntityManager();
+		
+//		String value = System.getProperty("name.variable");
+		
+		String jpql = "select c from Category c";
+		TypedQuery<Category> typedQuery = em.createQuery(jpql, Category.class);
+//		typedQuery.setParameter("variable", value);
+		
+		entityManagerFactory.addNamedQuery("Category.list", typedQuery);
+		
+	}
+	
+	@Test
+	void hybridApproach() {
+		TypedQuery<Category> typedQuery = entityManager
+			.createNamedQuery("Category.list", Category.class);
+		
+		List<Category> resultList = typedQuery.getResultList();
+		
+		assertFalse(resultList.isEmpty());
+	}
+	
+}
