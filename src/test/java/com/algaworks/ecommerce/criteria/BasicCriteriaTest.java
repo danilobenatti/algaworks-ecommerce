0c8be5cf2248da4c5b9a0f19451b97e94ecb6689
Danilo N. B. <danilobenatti@gmail.com>
2023-05-18 14:12:08 -0300

    Working with projections.

--- src/test/java/com/algaworks/ecommerce/criteria/BasicCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicCriteriaTest.java
@@ -1,15 +1,20 @@
 package com.algaworks.ecommerce.criteria;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Order;
 import com.algaworks.ecommerce.model.Person;
+import com.algaworks.ecommerce.model.Product;
 
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -17,6 +22,8 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 
 class BasicCriteriaTest extends EntityManagerTest {
+	
+	static Logger logger = Logger.getLogger(BasicCriteriaTest.class.getName());
 	
 	@Test
 	void searchById() {
@@ -69,4 +76,38 @@ class BasicCriteriaTest extends EntityManagerTest {
 		assertEquals(new BigDecimal("505.00"), total);
 	}
 	
+	@Test
+	void selectAllProducts() {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Product> query = builder.createQuery(Product.class);
+		Root<Product> root = query.from(Product.class);
+		
+		query.select(root);
+		
+		// String jpql = "select p from Product p"
+		
+		TypedQuery<Product> typedQuery = entityManager.createQuery(query);
+		List<Product> resultList = typedQuery.getResultList();
+		
+		assertFalse(resultList.isEmpty());
+	}
+	
+	@Test
+	void resultProjection() {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object[]> query = builder.createQuery(Object[].class);
+		Root<Product> root = query.from(Product.class);
+		
+		query.multiselect(root.get("id"), root.get("name"));
+		
+		// String jpql = "select p.id, p.name from Product p"
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(query);
+		List<Object[]> resultList = typedQuery.getResultList();
+		
+		resultList.forEach(o -> logger.log(Level.INFO, "{0}",
+			String.format("%s - %s", o[0], o[1])));
+		
+		assertFalse(resultList.isEmpty());
+	}
 }
