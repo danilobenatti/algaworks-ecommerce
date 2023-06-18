041d7ff4cdd449133422c1588f9ee33518ce4b33
Danilo N. B. <danilobenatti@gmail.com>
2023-06-18 14:10:31 -0300

    Lock with JPQL and Criteria API

--- src/test/java/com/algaworks/ecommerce/concurrence/LockPessimisticTest.java
+++ src/test/java/com/algaworks/ecommerce/concurrence/LockPessimisticTest.java
@@ -4,6 +4,7 @@ import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -11,6 +12,7 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.StringFormattedMessage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -251,6 +253,86 @@ class LockPessimisticTest {
 			default -> assertTrue(false);
 		}
 		log("Finalizing test method");
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	void usingLockNativeTypeQuery() {
+		Runnable runnable_1 = () -> {
+			log("Start Runnable 1");
+			
+			String newDescription = new StringBuilder()
+				.append("Descrição detalhada. CTM: ")
+				.append(System.currentTimeMillis()).toString();
+			
+			EntityManager em = entityManagerFactory.createEntityManager();
+			em.getTransaction().begin();
+			
+			log("Load Product by Runnable 1");
+			List<Product> list = em.createQuery("select p from Product p")
+				.setLockMode(PESSIMISTIC_READ).getResultList();
+			
+			Product product = list.stream().filter(p -> p.getId().equals(1L))
+				.findFirst().get();
+			
+			log("Change Product by Runnable 1");
+			product.setDescription(newDescription);
+			
+			log("Wait 3 seconds - Runnable 1");
+			waiting(3L, timeout);
+			
+			log("Transaction confirmation by Runnable 1");
+			em.getTransaction().commit();
+			
+			log("Ending Runnable 1");
+			em.close();
+		};
+		
+		Runnable runnable_2 = () -> {
+			log("Start Runnable 2");
+			
+			String newDescription = new StringBuilder()
+				.append("Descrição completa. CTM: ")
+				.append(System.currentTimeMillis()).toString();
+			
+			EntityManager em = entityManagerFactory.createEntityManager();
+			em.getTransaction().begin();
+			
+			log("Load Product by Runnable 2");
+			Product product = em.find(Product.class, 1L, PESSIMISTIC_WRITE);
+			
+			log("Change Product by Runnable 2");
+			product.setDescription(newDescription);
+			
+			log("Wait 1 seconds - Runnable 2");
+			waiting(1L, timeout);
+			
+			log("Transaction confirmation by Runnable 2");
+			em.getTransaction().commit();
+			
+			log("Ending Runnable 2");
+			em.close();
+		};
+		
+		Thread thread_1 = new Thread(runnable_1);
+		Thread thread_2 = new Thread(runnable_2);
+		
+		thread_1.start();
+		waiting(1L, timeout);
+		thread_2.start();
+		
+		try {
+			thread_1.join();
+			thread_2.join();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		
+		EntityManager em = entityManagerFactory.createEntityManager();
+		Product product = em.find(Product.class, 1L);
+		em.close();
+		
+		assertTrue(product.getDescription().startsWith("Descrição completa"));
 	}
 	
 }
